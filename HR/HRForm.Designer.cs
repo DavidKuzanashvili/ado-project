b4b346874a783b28751b9950f447aef4a0a2a494
Misho_Kupatadze <mishokupatadze@gmail.com>
2019-07-04 02:38:26 +0400

    Feature: Change Position

--- HR/HRForm.Designer.cs
+++ HR/HRForm.Designer.cs
@@ -44,6 +44,21 @@
             this.Mail = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.SaveEmployeeBtn = new System.Windows.Forms.Button();
+            this.employeeGridView = new System.Windows.Forms.DataGridView();
+            this.ChangePositionTitle = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ChnagePositionComboBox = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.ChangePositionPN = new System.Windows.Forms.TextBox();
+            this.changePositionBtn = new System.Windows.Forms.Button();
+            this.examTitle = new System.Windows.Forms.Label();
+            this.examNameInput = new System.Windows.Forms.TextBox();
+            this.examDescription = new System.Windows.Forms.TextBox();
+            this.maxScore = new System.Windows.Forms.TextBox();
+            this.examName = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.maxScoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // PositionDropDownList
@@ -202,12 +217,171 @@
             this.SaveEmployeeBtn.UseVisualStyleBackColor = true;
             this.SaveEmployeeBtn.Click += new System.EventHandler(this.SaveEmployeeBtn_Click);
             // 
+            // employeeGridView
+            // 
+            this.employeeGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.employeeGridView.Location = new System.Drawing.Point(295, 45);
+            this.employeeGridView.Name = "employeeGridView";
+            this.employeeGridView.Size = new System.Drawing.Size(511, 493);
+            this.employeeGridView.TabIndex = 18;
+            // 
+            // ChangePositionTitle
+            // 
+            this.ChangePositionTitle.AutoSize = true;
+            this.ChangePositionTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangePositionTitle.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ChangePositionTitle.Location = new System.Drawing.Point(891, 45);
+            this.ChangePositionTitle.Name = "ChangePositionTitle";
+            this.ChangePositionTitle.Size = new System.Drawing.Size(252, 24);
+            this.ChangePositionTitle.TabIndex = 19;
+            this.ChangePositionTitle.Text = "Change Employee\'s Position";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(892, 80);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Select Position";
+            // 
+            // ChnagePositionComboBox
+            // 
+            this.ChnagePositionComboBox.AutoCompleteCustomSource.AddRange(new string[] {
+            "Director",
+            "Secretary",
+            "ChiefOfDepartment",
+            "MainSpecialist",
+            "Specialist"});
+            this.ChnagePositionComboBox.FormattingEnabled = true;
+            this.ChnagePositionComboBox.Items.AddRange(new object[] {
+            "Director",
+            "Secretary",
+            "ChiefOfDepartment",
+            "MainSpecialist",
+            "Specialist"});
+            this.ChnagePositionComboBox.Location = new System.Drawing.Point(895, 106);
+            this.ChnagePositionComboBox.Name = "ChnagePositionComboBox";
+            this.ChnagePositionComboBox.Size = new System.Drawing.Size(175, 21);
+            this.ChnagePositionComboBox.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(892, 144);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "P / N";
+            // 
+            // ChangePositionPN
+            // 
+            this.ChangePositionPN.BackColor = System.Drawing.Color.White;
+            this.ChangePositionPN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangePositionPN.ForeColor = System.Drawing.Color.Black;
+            this.ChangePositionPN.Location = new System.Drawing.Point(895, 170);
+            this.ChangePositionPN.Name = "ChangePositionPN";
+            this.ChangePositionPN.Size = new System.Drawing.Size(175, 20);
+            this.ChangePositionPN.TabIndex = 23;
+            // 
+            // changePositionBtn
+            // 
+            this.changePositionBtn.Location = new System.Drawing.Point(935, 218);
+            this.changePositionBtn.Name = "changePositionBtn";
+            this.changePositionBtn.Size = new System.Drawing.Size(75, 23);
+            this.changePositionBtn.TabIndex = 24;
+            this.changePositionBtn.Text = "Change";
+            this.changePositionBtn.UseVisualStyleBackColor = true;
+            this.changePositionBtn.Click += new System.EventHandler(this.changePositionBtn_Click);
+            // 
+            // examTitle
+            // 
+            this.examTitle.AutoSize = true;
+            this.examTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.examTitle.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.examTitle.Location = new System.Drawing.Point(891, 292);
+            this.examTitle.Name = "examTitle";
+            this.examTitle.Size = new System.Drawing.Size(99, 24);
+            this.examTitle.TabIndex = 25;
+            this.examTitle.Text = "Add Exam";
+            // 
+            // examNameInput
+            // 
+            this.examNameInput.BackColor = System.Drawing.Color.White;
+            this.examNameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.examNameInput.ForeColor = System.Drawing.Color.Black;
+            this.examNameInput.Location = new System.Drawing.Point(895, 355);
+            this.examNameInput.Name = "examNameInput";
+            this.examNameInput.Size = new System.Drawing.Size(175, 20);
+            this.examNameInput.TabIndex = 26;
+            // 
+            // examDescription
+            // 
+            this.examDescription.BackColor = System.Drawing.Color.White;
+            this.examDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.examDescription.ForeColor = System.Drawing.Color.Black;
+            this.examDescription.Location = new System.Drawing.Point(895, 417);
+            this.examDescription.Name = "examDescription";
+            this.examDescription.Size = new System.Drawing.Size(175, 20);
+            this.examDescription.TabIndex = 27;
+            // 
+            // maxScore
+            // 
+            this.maxScore.BackColor = System.Drawing.Color.White;
+            this.maxScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxScore.ForeColor = System.Drawing.Color.Black;
+            this.maxScore.Location = new System.Drawing.Point(895, 480);
+            this.maxScore.Name = "maxScore";
+            this.maxScore.Size = new System.Drawing.Size(175, 20);
+            this.maxScore.TabIndex = 28;
+            // 
+            // examName
+            // 
+            this.examName.AutoSize = true;
+            this.examName.Location = new System.Drawing.Point(893, 324);
+            this.examName.Name = "examName";
+            this.examName.Size = new System.Drawing.Size(35, 13);
+            this.examName.TabIndex = 29;
+            this.examName.Text = "Name";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(893, 392);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(60, 13);
+            this.descriptionLabel.TabIndex = 30;
+            this.descriptionLabel.Text = "Description";
+            // 
+            // maxScoreLabel
+            // 
+            this.maxScoreLabel.AutoSize = true;
+            this.maxScoreLabel.Location = new System.Drawing.Point(892, 451);
+            this.maxScoreLabel.Name = "maxScoreLabel";
+            this.maxScoreLabel.Size = new System.Drawing.Size(58, 13);
+            this.maxScoreLabel.TabIndex = 31;
+            this.maxScoreLabel.Text = "Max Score";
+            // 
             // HRForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(848, 560);
+            this.ClientSize = new System.Drawing.Size(1210, 808);
+            this.Controls.Add(this.maxScoreLabel);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.examName);
+            this.Controls.Add(this.maxScore);
+            this.Controls.Add(this.examDescription);
+            this.Controls.Add(this.examNameInput);
+            this.Controls.Add(this.examTitle);
+            this.Controls.Add(this.changePositionBtn);
+            this.Controls.Add(this.ChangePositionPN);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.ChnagePositionComboBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.ChangePositionTitle);
+            this.Controls.Add(this.employeeGridView);
             this.Controls.Add(this.SaveEmployeeBtn);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.Mail);
@@ -227,6 +401,7 @@
             this.Name = "HRForm";
             this.Text = "HRForm";
             this.Load += new System.EventHandler(this.HRForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.employeeGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -249,5 +424,19 @@
         private System.Windows.Forms.TextBox Mail;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button SaveEmployeeBtn;
+        private System.Windows.Forms.DataGridView employeeGridView;
+        private System.Windows.Forms.Label ChangePositionTitle;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox ChnagePositionComboBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox ChangePositionPN;
+        private System.Windows.Forms.Button changePositionBtn;
+        private System.Windows.Forms.Label examTitle;
+        private System.Windows.Forms.TextBox examNameInput;
+        private System.Windows.Forms.TextBox examDescription;
+        private System.Windows.Forms.TextBox maxScore;
+        private System.Windows.Forms.Label examName;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Label maxScoreLabel;
     }
 }
--- HR/HRForm.cs
+++ HR/HRForm.cs
@@ -14,6 +14,8 @@ namespace HR
 {
     public partial class HRForm : Form
     {
+        private string conString = ConfigurationManager.ConnectionStrings["con_string"].ConnectionString;
+
         public HRForm()
         {
             InitializeComponent();
@@ -21,6 +23,32 @@ namespace HR
 
         private void HRForm_Load(object sender, EventArgs e)
         {
+            SqlConnection cn = new SqlConnection();
+            SqlCommand command;
+
+            cn.ConnectionString = conString;
+
+            try
+            {
+                cn.Open();
+                command = cn.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT * FROM EmployeeInfo";
+                command.ExecuteNonQuery();
+
+                DataTable employeeTable = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                da.Fill(employeeTable);
+                employeeGridView.DataSource = employeeTable;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Message: {ex.Message}");
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
 
         private void SaveEmployeeBtn_Click(object sender, EventArgs e)
@@ -30,12 +58,12 @@ namespace HR
             string surename = Surename.Text;
             string name = EmpName.Text;
             string mobileNumber = MobileNumber.Text;
-            string birthDay = BirthDay.Value.ToString("yyyy-MM-dd");
+            string birthDay = BirthDay.Value.ToString("yyyy-MM-dd hh:mm:ss");
             string mail = Mail.Text;
 
             //Positon table values
             string positionName = "";
-            string dateOfPositionHeld = DateTime.Now.ToString("yyyy-MM-dd");
+            string dateOfPositionHeld = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
 
             if(PositionDropDownList.SelectedItem != null)
             {
@@ -51,7 +79,7 @@ namespace HR
             SqlCommand command;
             SqlTransaction transaction;
 
-            cn.ConnectionString = ConfigurationManager.ConnectionStrings["con_string"].ConnectionString;
+            cn.ConnectionString = conString;
 
             //Action
             try
@@ -67,11 +95,11 @@ namespace HR
                     //Insert Employee
                     command.CommandType = CommandType.Text;
                     command.CommandText =
-                        $"INSERT INTO dbo.Employee(PN, Surname, Name, MobileNumber, BirthDate, Mail) VALUES (N'{personalNumber}', N'{surename}', N'{name}', N'{mobileNumber}', '{birthDay}', N'{mail}')";
+                        $"INSERT INTO dbo.Employee(PN, Surename, Name, BirthDate, MobileNumber, Mail) VALUES (N'{personalNumber}', N'{surename}', N'{name}', '{birthDay}', N'{mobileNumber}', N'{mail}')";
                     command.ExecuteNonQuery();
                     //Insert Employee Position
                     command.CommandText =
-                        $"INSERT INTO dbo.Position(PositionName, DateOfPositionHeld, PN) VALUES (N'{positionName}', '{dateOfPositionHeld}', N'{personalNumber}')";
+                        $"INSERT INTO dbo.Position(PN, PositionName, DateOfPositionHeld) VALUES (N'{personalNumber}', N'{positionName}', '{dateOfPositionHeld}')";
                     command.ExecuteNonQuery();
 
                     transaction.Commit();
@@ -96,6 +124,53 @@ namespace HR
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void changePositionBtn_Click(object sender, EventArgs e)
+        {
+            string personalNumber = ChangePositionPN.Text;
+            string posName = "";
+            string date = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+
+            if(ChnagePositionComboBox.SelectedItem != null)
+            {
+                posName = ChnagePositionComboBox.SelectedItem.ToString();
+            } 
+            else
+            {
+                MessageBox.Show("Please Select Position");
+            }
+
+            SqlConnection cn = new SqlConnection();
+            SqlCommand cmd = null;
+
+            cn.ConnectionString = conString;
+
+            try
+            {
+                cn.Open();
+                cmd = cn.CreateCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = $"INSERT INTO dbo.Position(PN, PositionName, DateOfPositionHeld) VALUES (N'{personalNumber}', N'{posName}', '{date}')";
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Position Changed!");
+
+                ChangePositionPN.Text = "";
+                ChnagePositionComboBox.SelectedItem = null;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Message: {ex.Message}");
+            }
+            finally
+            {
+                cn.Close();
             }
         }
     }
